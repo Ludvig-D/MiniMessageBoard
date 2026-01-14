47a687502882fde8dae6c3e6e31dc7a58180e083
Ludvig <74721111+Ludvig-D@users.noreply.github.com>
2026-01-14 17:16:07 +0100

    Update fixed connection to db

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,10 +1,5 @@
 import { Pool } from 'pg';
 
 export default new Pool({
-  host: process.env.SQLHOST,
-  user: process.env.SQLUSER,
-  database: process.env.SQLDATABASE,
-  password: process.env.SQLPASSWORD,
-  port: process.env.SQLPORT,
-  // connectionString: process.env.SQLDBCONNECTIONURL,
+  connectionString: process.env.DATABASE_URL,
 });
